697fe4196ba525a41c1c417661691462ed8c9c06
Thomas Paulson <thomasdolar@gmail.com>
2018-09-23 07:25:21 +0000

    udpate seed file

--- lib/machine_monitor/repo.ex
+++ lib/machine_monitor/repo.ex
@@ -8,4 +8,11 @@ defmodule MachineMonitor.Repo do
   def init(_, opts) do
     {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
   end
+
+  @spec truncate(Ecto.Schema.t()) :: :ok
+  def truncate(schema) do
+    table_name = schema.__schema__(:source)
+    query("TRUNCATE #{table_name}", [])
+    :ok
+  end
 end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,20 +10,40 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
+alias MachineMonitor.{Repo, Accounts, Settings}
+
+# seed root user
+Repo.truncate(Accounts.User)
 MachineMonitor.Accounts.create_user!(
   %{email: "admin@admin.admin", name: "Mr Admin", password: "adminadmin"}
 )
 
-Enum.each([
-  %{name: "EnrollmentStation", display: "Dermalog Enrollment Station",
-    path: "C:\\Program Files (x86)\\DERMALOG\\EnrollmentStation\\EnrollmentStation.exe"},
-  %{name: "Dermalog Card Issuing Station", display: "Dermalog Card Issuing Station",
-    path: "C:\\Program Files (x86)\\DERMALOG\\Dermalog Card Issuing Station\\CardIssuingStation.exe"},
-], fn app -> MachineMonitor.Settings.create_application(app) end)
+# seed pages
 
-Enum.each([
-  %{name: "CardReaderService", display: "Dermalog Card Reader Service",
-    path: "C:\\Program Files (x86)\\DERMALOG\\CardReaderService\\CardReaderServiceWrapper.exe"},
-  %{name: "CodeMeter.exe", display: "CodeMeter Runtime Server",
-    path: "C:\\Program Files (x86)\\CodeMeter\\Runtime\\bin\\CodeMeter.exe"},
-], fn service -> MachineMonitor.Settings.create_service(service) end)
+# seed entities
+
+# seed actions
+
+if Mix.env() in [:dev, :test] do
+
+  # seed applications | test purpose only
+  Repo.truncate(Settings.Application)
+  Enum.each([
+    %{name: "EnrollmentStation", display: "Dermalog Enrollment Station",
+      path: "C:\\Program Files (x86)\\DERMALOG\\EnrollmentStation\\EnrollmentStation.exe"},
+    %{name: "Dermalog Card Issuing Station", display: "Dermalog Card Issuing Station",
+      path: "C:\\Program Files (x86)\\DERMALOG\\Dermalog Card Issuing Station\\CardIssuingStation.exe"},
+  ], fn app -> MachineMonitor.Settings.create_application(app) end)
+
+  # seed services | test purpose only
+  Repo.truncate(Settings.Service)
+  Enum.each([
+    %{name: "CardReaderService", display: "Dermalog Card Reader Service",
+      path: "C:\\Program Files (x86)\\DERMALOG\\CardReaderService\\CardReaderServiceWrapper.exe"},
+    %{name: "CodeMeter.exe", display: "CodeMeter Runtime Server",
+      path: "C:\\Program Files (x86)\\CodeMeter\\Runtime\\bin\\CodeMeter.exe"},
+  ], fn service -> MachineMonitor.Settings.create_service(service) end)
+
+  #seed centers | test purpose only
+
+end
